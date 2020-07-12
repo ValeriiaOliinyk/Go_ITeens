bea4c47f77d6c15258edad3daae2a3754ccbbe29
Valeriia Oliinyk <olijnykvaleria@gmail.com>
2020-07-12 13:54:03 +0300

    added files

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,7 +1,7 @@
 const anchors = document.querySelectorAll('a[href*="#"]');
 
 for (let anchor of anchors) {
-  anchor.addEventListener('click', function(event) {
+  anchor.addEventListener('click', function (event) {
     event.preventDefault();
     const blockID = anchor.getAttribute('href');
     document.querySelector('' + blockID).scrollIntoView({
